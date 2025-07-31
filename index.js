fcfdf8d821540ff1f3fa93462c9e71f713acffb2
UlugbekAzimov <106437785+UlugbekAzimov@users.noreply.github.com>
2025-07-31 09:22:37 +0500

    index.js

--- index.js
+++ index.js
@@ -17,22 +17,3 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(5000, () => {
   console.log('🚀 Server 5000-portda ishga tushdi');
 });
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const productRoutes = require('./routes/productRoutes');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use('/api/products', productRoutes);
-
-// MongoDB Atlas ulanishi
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('✅ MongoDB Atlas bilan ulandi!'))
-  .catch(err => console.error('❌ Ulanishda xatolik:', err));
-
-app.listen(5000, () => {
-  console.log('🚀 Server 5000-portda ishga tushdi');
-});
