653dac8dac64fa10fd54f6a0c9eba428b20c06a1
UlugbekAzimov <106437785+UlugbekAzimov@users.noreply.github.com>
2025-08-06 21:39:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 require('dotenv').config();
 
-
 const app = express();
-app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
 app.use(express.json());
 
-// ROUTES
-const productRoutes = require('./routes/products');
-const authRoutes = require('./routes/auth'); // 🟢 YANGI
-
-app.use('/api/products', productRoutes);
-app.use('/api', authRoutes); // 🟢 /api/login endi shu orqali ishlaydi
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB ulandi'))
-  .catch(err => console.error('MongoDB ulanishda xatolik:', err));
-
-app.listen(5000, () => console.log('Server 5000-portda'));
-
-
+// MongoDB ulanishi
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB ulandi'))
+.catch((err) => console.error('MongoDB ulanishda xatolik:', err));
 
 // 🔽 Bu joy muhim!
 app.get('/', (req, res) => {
